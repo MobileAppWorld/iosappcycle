b662b3ff858cde2ca576220cd35a526c0b51e9e5
venkatesh <venkatesha.murthy@aatmunn.com>
2025-07-12 21:01:55 +0530

    inital commit

---
+++ iosappcycle/iosappcycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosappcycleApp.swift
+//  iosappcycle
+//
+//  Created by Venkatesh on 12/07/25.
+//
+
+import SwiftUI
+
+@main
+struct iosappcycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iosappcycleTests/iosappcycleTests.swift
@@ -0,0 +1,16 @@
+//
+//  iosappcycleTests.swift
+//  iosappcycleTests
+//
+//  Created by Venkatesh on 12/07/25.
+//
+
+import Testing
+
+struct iosappcycleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
